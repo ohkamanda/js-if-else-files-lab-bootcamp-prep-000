1e1cf534552a987beb963d78e5c51e7661c1b1c4
Amanda P <ohkamanda@gmail.com>
2018-08-04 16:30:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,13 +1,6 @@
-var name = "Susan"
+var name = "Joe"
 var height = "74"
-
-if (name === "Susan") {
-  console.log("Correct name")
-} else {
-  console.log("Incorrect")
-}
-
-
+ 
 // Don't worry about this
 module.exports = { name, height
 }
